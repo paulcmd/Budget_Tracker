692f3cd21a6e8e519bccb3328ea847f66fbbd3e2
paulcmd <choxmail.pm@gmail.com>
2020-07-14 16:46:09 -0700

    Added delete buttons and math signs

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,4 +3,18 @@ import React from "react";
 export const Transaction = ({ transaction, deleteTransaction }) => {
   const sign = transaction.amount < 0 ? "-" : "+";
 
-
+  return (
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+      {transaction.text}{" "}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+      <button
+        onClick={() => deleteTransaction(transaction.id)}
+        className="delete-btn"
+      >
+        x
+      </button>
+    </li>
+  );
+};
