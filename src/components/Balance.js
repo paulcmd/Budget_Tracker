559d8b345bc02a1a3a49ea2e0fdb7f78d0109af5
paulcmd <choxmail.pm@gmail.com>
2020-07-14 16:53:34 -0700

    accumulate amounts in Balance

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,6 +4,7 @@ export const Balance = ({transactions}) => {
 
   const amounts = transactions.map(transaction => transaction.amount);
 
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
   return (
     <>
